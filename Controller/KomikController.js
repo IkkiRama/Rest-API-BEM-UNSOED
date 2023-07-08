02187ad3e91d82f6aa862bde73f4f0f58a72e8fd
IkkiRama <rifkivamaus@gmail.com>
2023-07-08 18:25:12 +0700

    Update method select & detail di komikcontroller

--- Controller/KomikController.js
+++ Controller/KomikController.js
@@ -5,23 +5,103 @@ import conn from "../koneksi.js";
 class KomikController {
   constructor() {}
   Select = (req, res) => {
-    conn.query(`SELECT * FROM komik`, (err, result) => {
-      if (err) {
-        response(err, res);
-      } else {
-        response(result, res);
-      }
+    const query = `
+      SELECT komik.id_komik, komik.id_user, komik.title_komik, user.id_user, user.nama, user.username, user.alamat, user.deskripsi, galeriKomik.id_galerikomik, galeriKomik.gambar
+      FROM komik
+      INNER JOIN user ON komik.id_user = user.id_user
+      INNER JOIN galeriKomik ON komik.id_komik = galeriKomik.id_komik
+    `;
+
+    conn.query(query, (error, results) => {
+      if (error) throw error;
+
+      // Mengonversi hasil query menjadi skema JSON yang diinginkan
+      const komiks = results.reduce((acc, row) => {
+        const existingKomik = acc.find(
+          (komik) => komik.id_komik === row.id_komik
+        );
+
+        if (existingKomik) {
+          existingKomik.galeri.push({
+            id_galeriKomik: row.id_galerikomik,
+            id_komik: row.id_komik,
+            gambar: row.gambar,
+          });
+        } else {
+          const newKomik = {
+            id_komik: row.id_komik,
+            id_user: row.id_user,
+            title_komik: row.title_komik,
+            author: {
+              id_user: row.id_user,
+              nama: row.nama,
+              username: row.username,
+              alamat: row.alamat,
+              deskripsi: row.deskripsi,
+            },
+            galeri: [
+              {
+                id_galeriKomik: row.id_galerikomik,
+                id_komik: row.id_komik,
+                gambar: row.gambar,
+              },
+            ],
+          };
+
+          acc.push(newKomik);
+        }
+
+        return acc;
+      }, []);
+
+      response(komiks, res);
     });
+
+    // conn.query(`SELECT * FROM komik`, (err, result) => {
+    //   if (err) {
+    //     response(err, res);
+    //   } else {
+    //     response(result, res);
+    //   }
+    // });
   };
 
   Detail = (req, res) => {
     let id = req.params.id;
-    conn.query(`SELECT * FROM komik WHERE id_komik = ${id}`, (err, result) => {
-      if (err) {
-        response(err, res);
-      } else {
-        response(result, res);
-      }
+
+    const query = `
+    SELECT komik.id_komik, komik.id_user, komik.title_komik, user.id_user, user.nama, user.username, user.alamat, user.deskripsi, galeriKomik.id_galerikomik, galeriKomik.gambar
+    FROM komik
+    INNER JOIN user ON komik.id_user = user.id_user
+    INNER JOIN galeriKomik ON komik.id_komik = galeriKomik.id_komik
+    WHERE komik.id_komik=${id}
+  `;
+
+    conn.query(query, (error, results) => {
+      if (error) throw error;
+
+      const dataQuery = results[0];
+
+      // Mengonversi hasil query menjadi skema JSON yang diinginkan
+      const komik = {
+        id_komik: dataQuery.id_komik,
+        id_user: dataQuery.id_user,
+        title_komik: dataQuery.title_komik,
+        author: {
+          id_user: dataQuery.id_user,
+          nama: dataQuery.nama,
+          username: dataQuery.username,
+          alamat: dataQuery.alamat,
+          deskripsi: dataQuery.deskripsi,
+        },
+        galeri: results.map((row) => ({
+          id_galeriKomik: row.id_galerikomik,
+          id_komik: row.id_komik,
+          gambar: row.gambar,
+        })),
+      };
+
+      response(komik, res);
     });
   };
 
